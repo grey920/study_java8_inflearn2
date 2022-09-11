774b59808807460dde58f35bab5eba5abe1a9c94
grey <charminggw@gmail.com>
2022-09-12 00:28:15 +0900

    람다표현식

--- src/main/java/me/whiteship/java8to11/Foo.java
+++ src/main/java/me/whiteship/java8to11/Foo.java
@@ -3,38 +3,64 @@ package me.whiteship.java8to11;
 import java.util.function.*;
 
 public class Foo {
+    static int classBaseNum = 20; //클래스 변수
+    public int instanceBaseNum = 10;
 
     public static void main ( String[] args ) {
 
-        /* Function<T,R>*/
-        Function<Integer, Integer> plus5 = ( i ) -> i + 5;
-        Function<Integer, Integer> multiply5 = ( i ) -> i * 5;
-
-        Function<Integer, Integer> plus5AndMultiply5 = plus5.andThen( multiply5 );
-        System.out.println( plus5AndMultiply5.apply( 2 ) );
-
-        Function<Integer, Integer> plus5AndcomposeMultiply5 = plus5.compose( multiply5 );
-        System.out.println( plus5AndcomposeMultiply5.apply( 2 ) );
-
-        /* UnaryOperator: 입력값과 리턴값이 같은 타입일 경우 */
-        UnaryOperator<Integer> subtract2 = ( i ) -> i - 2;
-        System.out.println( "subtract2 = " + subtract2.apply( 5 ) );
-
-        /*Predicate*/
-        Predicate<Integer> isOdd = ( i ) -> i % 2 != 0;
-        System.out.println( isOdd.test( 4 ) );
-        // negate(): 반대의 값을 리턴하는 메서드
-        Predicate<Integer> negate = isOdd.negate();
-        System.out.println( negate.test( 4 ) );
-
-        /*Supplier*/
-        Supplier<String> nameSupplier = () -> "I'm grey~";
-        System.out.println( nameSupplier.get() );
-
-        /*Consumer*/
-        Consumer<String> nameConsumer = ( str ) -> System.out.println( "I'm " + str );
-        nameConsumer.accept( "grey!!!!!" );
-
+        Foo foo = new Foo();
+        foo.run();
 
     }
+
+    private void run () {
+
+        int baseNum = 10;
+
+        IntConsumer printInt = ( i ) -> {
+            System.out.println( i + instanceBaseNum );
+            instanceBaseNum++;
+            classBaseNum++;
+//            baseNum++; // <- [ERR] 컴파일 에러!!!
+
+            System.out.println("instanceBaseNum : " + instanceBaseNum );
+            System.out.println("classBaseNum : " + classBaseNum );
+        };
+
+        printInt.accept( 5 );
+    }
+
+
+
+
+    private void run2 () {
+
+        int baseNum = 10;
+
+        /* 로컬 클래스 */
+        class LocalClass{
+            void printBaseNum( int baseNum ){
+                System.out.println( baseNum );
+            }
+        }
+
+        /* 익명 클래스 */
+        Consumer<Integer> integerConsumer = new Consumer<Integer>() {
+            @Override
+            public void accept ( Integer integer ) {
+
+                System.out.println( baseNum );
+            }
+        };
+
+        IntConsumer printInt = ( i ) -> {
+            // 여기서 참조하는 로컬 변수인 baseNum은 캡쳐가 된다. (변수 캡쳐 variable capture)
+//            int baseNum = 20;
+            System.out.println( i + baseNum );
+        };
+
+        integerConsumer.accept( 5 );
+        printInt.accept( 5 );
+    }
+
 }
