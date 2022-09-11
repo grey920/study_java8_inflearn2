af68bb2d04fc0e883d898e237daf4cf490d7b4eb
grey <charminggw@gmail.com>
2022-09-11 22:13:47 +0900

    자바가 기본적으로 제공하는 함수 인터페이스

--- src/main/java/me/whiteship/java8to11/Foo.java
+++ src/main/java/me/whiteship/java8to11/Foo.java
@@ -1,19 +1,39 @@
 package me.whiteship.java8to11;
 
+import java.util.function.*;
+
 public class Foo {
 
     public static void main ( String[] args ) {
 
-        RunSomthing runSomthing = new RunSomthing() {
-            int baseNumber = 10;
+        /* Function<T,R>*/
+        Function<Integer, Integer> plus5 = ( i ) -> i + 5;
+        Function<Integer, Integer> multiply5 = ( i ) -> i * 5;
 
-            @Override
-            public int doIt ( int number ) {
-                // 문법적으로는 가능하지만 순수한 함수라고 할 수 없다.
-                baseNumber++;
-                return number + baseNumber;
-            }
-        };
+        Function<Integer, Integer> plus5AndMultiply5 = plus5.andThen( multiply5 );
+        System.out.println( plus5AndMultiply5.apply( 2 ) );
+
+        Function<Integer, Integer> plus5AndcomposeMultiply5 = plus5.compose( multiply5 );
+        System.out.println( plus5AndcomposeMultiply5.apply( 2 ) );
+
+        /* UnaryOperator: 입력값과 리턴값이 같은 타입일 경우 */
+        UnaryOperator<Integer> subtract2 = ( i ) -> i - 2;
+        System.out.println( "subtract2 = " + subtract2.apply( 5 ) );
+
+        /*Predicate*/
+        Predicate<Integer> isOdd = ( i ) -> i % 2 != 0;
+        System.out.println( isOdd.test( 4 ) );
+        // negate(): 반대의 값을 리턴하는 메서드
+        Predicate<Integer> negate = isOdd.negate();
+        System.out.println( negate.test( 4 ) );
+
+        /*Supplier*/
+        Supplier<String> nameSupplier = () -> "I'm grey~";
+        System.out.println( nameSupplier.get() );
+
+        /*Consumer*/
+        Consumer<String> nameConsumer = ( str ) -> System.out.println( "I'm " + str );
+        nameConsumer.accept( "grey!!!!!" );
 
 
     }
