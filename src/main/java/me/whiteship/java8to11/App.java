e7883207f216eae526399c8bf19756b8690a508c
grey <charminggw@gmail.com>
2022-09-14 07:42:12 +0900

    메서드 레퍼런스

--- src/main/java/me/whiteship/java8to11/App.java
+++ src/main/java/me/whiteship/java8to11/App.java
@@ -1,25 +1,46 @@
 package me.whiteship.java8to11;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Random;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 import java.util.stream.IntStream;
 
 public class App {
 
     public static void main(String[] args) {
-        int size = 1500;
-        int[] numbers = new int[size];
-        Random random = new Random();
 
-        IntStream.range(0, size).forEach(i -> numbers[i] = random.nextInt());
-        long start = System.nanoTime();
-        Arrays.sort(numbers);
-        System.out.println("serial sorting took " + (System.nanoTime() - start));
+        /* 람다식 직접 구현 */
+        UnaryOperator<String> str = ( s ) -> "hi " + s;
+        System.out.println(str.apply( "grey" ));
 
-        IntStream.range(0, size).forEach(i -> numbers[i] = random.nextInt());
-        start = System.nanoTime();
-        Arrays.parallelSort(numbers);
-        System.out.println("parallel sorting took " + (System.nanoTime() - start));
+        /* 1. static 메서드 - 객체 타입::static 메소드 */
+        UnaryOperator<String> hi = Greeting::hi;
+
+        /* 2. 특정 객체의 instance 메서드 - 객체 레퍼런스::인스턴스 메소드 */
+        Greeting greeting = new Greeting();
+        UnaryOperator<String> hello = greeting::hello;
+
+        /* 3. 생성자 - 객체 타입::new */
+        Supplier<Greeting> greetingSupplier = Greeting::new;
+        Function<String, Greeting> greetingFunction = Greeting::new;
+        Greeting grey = greetingFunction.apply( "grey" );
+        System.out.println(grey.getName());
+
+        /* 4. 임의 객체의 instance 메서드 - 객체 타입::인스턴스 메소드 */
+        // 특정 타입 불특정 다수인 인스턴스
+        String[] names = { "grey", "gyuwoon", "apple"};
+//        Arrays.sort( names, ( o1, o2 ) -> 0 );
+
+        // compareToIgnoreCase : 어떤 문자열을 비교해서 int 값을 반환하는 메서드
+//        public int compareToIgnoreCase(String str) {
+//            return CASE_INSENSITIVE_ORDER.compare(this, str);
+//        }
+        Arrays.sort( names, String::compareToIgnoreCase );
+
+        System.out.println( "Arrays.toString( names ) = " + Arrays.toString( names ) );
 
     }
 
