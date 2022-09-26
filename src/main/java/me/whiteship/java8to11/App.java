c5d728deb19f099333a389f563747afb6771218c
grey <charminggw@gmail.com>
2022-09-27 07:19:01 +0900

    Date 실습 끝

--- src/main/java/me/whiteship/java8to11/App.java
+++ src/main/java/me/whiteship/java8to11/App.java
@@ -1,6 +1,7 @@
 package me.whiteship.java8to11;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.function.Predicate;
@@ -11,6 +12,7 @@ public class App {
 
     public static void main ( String[] args ) throws InterruptedException {
 
+        System.out.println( "=============== " );
         /* 사람용 vs 기계용 */
         // 기계적 시간 API
         Instant instant = Instant.now(); // of() => 특정 에폭 시간을 기준으로 만든다
@@ -24,6 +26,7 @@ public class App {
         System.out.println( "zonedDateTime = " + zonedDateTime );
 
         //================
+        System.out.println( "===============" );
         // 사람용 시간 API
         LocalDate now = LocalDate.now(); // 해당 소스가 돌아가는 서버의 시스템 zone 시간대를 사용한다
         System.out.println( "now = " + now );
@@ -38,6 +41,7 @@ public class App {
         /* Instant <-> ZonedDateTime <-> LocalDateTime */
 
         //===============
+        System.out.println( "===============" );
         // 기간을 나타내는 두 가지 -> Duration(시간 기반- 기계용), Period(날짜 기반 - 사람용)
         LocalDate today = LocalDate.now();
         LocalDate nextYearBirthday = LocalDate.of( 2023, Month.JANUARY, 18 );
@@ -53,11 +57,57 @@ public class App {
         System.out.println( ChronoUnit.DAYS.between( today, nextYearBirthday ) );
 
 
-        Instant now1 = Instant.now();
-        LocalDate plus = now.plus( 10, ChronoUnit.SECONDS ); // <-  Unsupported unit: Seconds ERROR!
-        Duration between = Duration.between( now1, plus );
+        Instant nowInstantForBetween = Instant.now();
+        Instant plus = nowInstantForBetween.plus( 10, ChronoUnit.SECONDS );
+        Duration between = Duration.between( nowInstantForBetween, plus );
         System.out.println( "between = " + between.getSeconds() );
 
+        //===============
+        System.out.println( "===============" );
+        /* formatting : Date -> text  */
+        LocalDateTime localDateTimeNow = LocalDateTime.now();
+        System.out.println( "localDateTimeNow = " + localDateTimeNow );
+        // 원하는 모양으로 출력하고 싶을 때 DateTimeFormatter (문자열로 변환)
+        // 미리 정의되어있는 타입이라면 문서 참고 https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html#predefined
+        DateTimeFormatter MMddyyyy = DateTimeFormatter.ofPattern( "MM/dd/yyyy" );
+        System.out.println( "Formatted localDateTimeNow = " + localDateTimeNow.format( MMddyyyy ) );
+
+        /* parsing : text -> Date */
+        LocalDate parse = LocalDate.parse( "09/27/2022" , MMddyyyy );
+        System.out.println( "parse = " + parse );
+
+
+        //===============
+        System.out.println( "===============" );
+        /* 레거시 API 지원: Instant로만 변환할 수 있으면 최신 API로 다 바꿀 수 있다 */
+        // Date(old) <-> Instant(new)
+        Date date = new Date();
+        Instant toInstant = date.toInstant(); // date -> instant
+        Date toDate = Date.from( toInstant ); // instant -> date
+
+        System.out.println( "toInstant = " + toInstant );
+        System.out.println( "toDate = " + toDate );
+
+        // GregorianCalendar(old) <-> ZonedDateTime, LocalDateTime(new)
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        ZonedDateTime dateTime = gregorianCalendar.toInstant().atZone( ZoneId.systemDefault() ); // GregorianCalendar -> ZonedDateTime
+        System.out.println( "dateTime = " + dateTime );
+        GregorianCalendar from = GregorianCalendar.from( dateTime ); // ZonedDateTime -> GregorianCalendar
+        System.out.println( "from = " + from.getTime() );
+
+        // TimeZone(oid) <-> ZoneId(new)
+        ZoneId zoneId = TimeZone.getTimeZone( "PST" ).toZoneId(); // TimeZone(oid) -> ZoneId(new)
+        System.out.println( "zoneId = " + zoneId );
+        TimeZone timeZone = TimeZone.getTimeZone( zoneId ); // ZoneId -> TimeZone
+        System.out.println( "timeZone = " + timeZone.getID() );
+
+        System.out.println( "===============" );
+        // 연산하기
+        LocalDateTime localDateTime = LocalDateTime.now();
+        // [Tip!] plus()의 두번째 인자로 TemporalUnit을 넣게 되어있지만, TemporalUnit을 쓰면 더 쓸 수 있는게 없기 때문에 대신 ChronoUnit을 사용하면 편하다
+        // [Warning!] localDateTime은 Immutable하기 때문에 반드시 인스턴스를 새로 만든 것으로 사용해야 한다!!! 옛날 API처럼 localDateTime.plus()만 쓰고 끝내면 아무 동작도 하지 않는다.
+        LocalDateTime localDatePlus = localDateTime.plus( 10, ChronoUnit.DAYS );
+        System.out.println( "localDatePlus = " + localDatePlus );
 
     }
 
