a4bbe7d549cb29696bfae0ab8c39c1454db0f9e4
grey <charminggw@gmail.com>
2022-09-30 23:55:40 +0900

    Concurrent Programming - Executor : single thread shutdown

--- src/main/java/me/whiteship/java8to11/ExecutorApp.java
+++ src/main/java/me/whiteship/java8to11/ExecutorApp.java
@@ -10,11 +10,20 @@ public class ExecutorApp {
         ExecutorService executorService = Executors.newSingleThreadExecutor(); //Thread 하나만 사용
 
         // 1. 가장 고전적인 방법
-        executorService.execute( new Runnable() {
-            @Override
-            public void run () {
-                System.out.println( "Thread ::: " + Thread.currentThread().getName() );
-            }
+//        executorService.execute( new Runnable() {
+//            @Override
+//            public void run () {
+//                System.out.println( "Thread ::: " + Thread.currentThread().getName() );
+//            }
+//        } );
+
+        // 2. submit 사용
+        executorService.submit( () -> {
+            System.out.println( "Thread ::: " + Thread.currentThread().getName() );
         } );
+
+        /* 위에서 실행만 하므로 계속 돌고 있기 때문에 작업 후에는 명시적으로 shutdown을 해줘야 한다. */
+        executorService.shutdown(); // graceful shutdown (현재 진행중인 작업을 끝까지 마치고 죽는다)
+//        executorService.shutdownNow(); // 실행중인 작업을 바로 죽인다
     }
 }
