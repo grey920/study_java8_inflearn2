c730447a3b0e6816a693b93c77a88d5cf884011b
grey <charminggw@gmail.com>
2022-10-05 08:35:22 +0900

    Concurrent Programming - CompletableFuture2: 조합하기

--- src/main/java/me/whiteship/java8to11/CompletableFutureApp.java
+++ src/main/java/me/whiteship/java8to11/CompletableFutureApp.java
@@ -32,7 +32,7 @@ public class CompletableFutureApp {
         System.out.println( future2.get() );
 
 
-        // 3. 실제로 어떤 작업을 하고싶은 경우 리턴이 있는 경우와 없는 경우로 나눌 수 있다.
+        // 3. 비동기로 작업 실행하기: 실제로 어떤 작업을 하고싶은 경우 리턴이 있는 경우와 없는 경우로 나눌 수 있다.
         // 3-1. 리턴이 없는 경우 -> runAsync()
         // 리턴값이 없어서 Generic이 Void가 된다.
         CompletableFuture<Void> future3 = CompletableFuture.runAsync( () -> {
@@ -49,7 +49,11 @@ public class CompletableFutureApp {
         System.out.println(future4.get()); // future4.get()을 "할 때" 위의 supplyAsync()가 수행된다
 
 
-        /* callback 주기 => thenApply()를 주면 받은 결과값을 다른 타입으로 변경한다
+        /* 4. 콜백 제공하기
+        * [콜백 종류]
+        * - thenApply() : 받은 결과값을 다른 타입으로 변경한다
+        * - thenAccept() : 리턴값을 또 다른 작업을 처리한다 (리턴X)
+        * - thenRun() : 리턴값을 받지 않고 또 다른 작업을 처리한다
         * 그래도 get()은 호출해야 한다
         * (중요!) 기존 Java5에 Future는 get() 이전에 콜백을 정의해놓는게 불가능했는데 CompletableFuture에서는 .thenApply()로 get()전에 미리 콜백을 써놓는게 가능해졌다!!!
          */
@@ -82,7 +86,7 @@ public class CompletableFutureApp {
         future7.get();
 
 
-        // 4. Executors 사용해서 쓰레드풀 직접 만들어서 주기
+        // Executors 사용해서 쓰레드풀 직접 만들어서 주기
         // 위의 예처럼 ForkJoinPool의 commonPool이 아니라 pool-1-thread-1이 출력된다
         ExecutorService executorService = Executors.newFixedThreadPool( 4 );
         // 두번째 인자로 ExecutorService를 줄 수 있다
@@ -95,6 +99,53 @@ public class CompletableFutureApp {
         future8.get();
         executorService.shutdown();
 
+
+        System.out.println(":::::::::::::::::::: 조합하기 ::::::::::::::::::");
+
+        /* 5. 조합하기
+        * */
+        CompletableFuture<String> hello = CompletableFuture.supplyAsync( () -> {
+            System.out.println( "Hello 9 " + Thread.currentThread().getName() );
+            return "Hello";
+        } );
+
+        // 이전의 경우, hello.get()을 하고 기다렸다가 world.get()을 해야했다.
+//        hello.get();
+//        world.get();
+
+        // thenCompose() : 이 뒤에 추가적인 작업을 이어서 할 수 있다 ( 서로 의존성, 연관관계가 있는 경우 사용. 즉 순서가 정해져있는 경우 )
+        CompletableFuture<String> future9_1 = hello.thenCompose( CompletableFutureApp::getWorld );// hello의 결과를 받아서
+        System.out.println( "future9_1 thenCompose = " + future9_1.get() );
+
+
+
+        // thenCombine() : 서로 연관관계가 없는 경우 비동기적인 작업을 동시에 처리하는 방법
+        // ( 예, A는 애플 주식 정보 조회하고 B는 삼성 주식 조회할 때 따로 가져오는 경우 -> A 조회 끝날때까지 B가 기다릴 필요없이 A와 B 모두 던져놓고 둘 다 결과가 왔을 때 뭔가를 하고싶은 경우 사용 )
+        CompletableFuture<String> future9_2 = hello.thenCombine( getWorld("Hello"), ( h, w ) -> h + " " + w );// BiFunction을 사용
+        System.out.println( "future9_2 thenCombine = " + future9_2.get() );
+
+
+        // allOf() : 2개 이상일 때 여러 task들을 다 합쳐서 실행
+        // allOf에 넘어간 일들이 다 끝났을 때 .thenApply나 thenAccept 등 추가적인 콜백을 수행할 수 있다.
+        // [주의!!] allOf의 인자가 같은 타입이라는 보장도 없고, 수행 중에 에러가 날 수도 있다. 아래 예시는 인자의 타입이 다른 경우라 null이 출력된다
+        CompletableFuture<Void> future9_3 = CompletableFuture.allOf( hello, getIntegerWorld( 1 ) ).thenAccept( System.out::println );
+        System.out.println( "future9_3 = " + future9_3.get() );
+
+
+    }
+
+    private static CompletableFuture<String> getWorld ( String msg ) {
+        return CompletableFuture.supplyAsync( () -> {
+            System.out.println( msg + "World 9 " + Thread.currentThread().getName() );
+            return msg + " World";
+        } );
+    }
+
+    private static CompletableFuture<Integer> getIntegerWorld ( Integer integer ) {
+        return CompletableFuture.supplyAsync( () -> {
+            System.out.println( integer + "World " + Thread.currentThread().getName() );
+            return integer;
+        } );
     }
 
 }
